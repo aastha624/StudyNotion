877a39e303f5a17cd78e2d7a41ef69552c88f4e0
aastha624 <patelaastha2910@gmail.com>
2024-09-08 23:51:22 +0530

    almost done some error occur slove and comming soon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import Login from "./components/Login";
+import Home from "./components/Home.js";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 
-const App = () => {
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <div>
-      Hello World
+    <div className="w-screen h-screen bg-richblack-900 flex flex-col ">
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/signup"
+          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <PrivateRoute isLoggedIn={isLoggedIn}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </PrivateRoute>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+// src/App.js
+
 
